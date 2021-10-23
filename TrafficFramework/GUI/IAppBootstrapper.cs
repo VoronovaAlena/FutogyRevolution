59e1093a5fe9fe7e053202e46674774715b2c8b5
voronova <voronova.alyon@mallenom.ru>
2021-10-23 16:52:04 +0300

    add ui

---
+++ TrafficFramework/GUI/IAppBootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TrafficFramework.GUI
+{
+	public interface IAppBootstrapper : IDisposable
+	{
+		/// <summary>Асинхронно запускает приложение.</summary>
+		/// <remarks>Задача, представляющая асинхронную операцию запуска приложения.</remarks>
+		Task RunAsync();
+	}
+}
